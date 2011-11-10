88616f6eaaef1f38d721dcecfae18ecf15b41711
naokirin <malfunctional-ai@sings.jp>
2011-11-10 19:42:32 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject TwitterMemorizerLib "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "[1.2.1]"]
+                   [org.clojure/clojure-contrib "[1.2,)"]
+                   [org.twitter4j/twitter4j-core "[1.2,)"]]
+  :aot [TwitterMemorizerLib.Oauth])
