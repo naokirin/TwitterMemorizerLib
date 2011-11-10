d7560699aa8edd2f7e1f1a8b4943670b1cf4779a
naokirin <malfunctional-ai@sings.jp>
2011-11-10 19:44:05 +0900

    TwitterのOAuth認証を行うための機能を実装した

---
+++ src/TwitterMemorizerLib/Oauth.clj
@@ -0,0 +1,37 @@
+(ns TwitterMemorizerLib.Oauth
+  (:import [twitter4j.TwitterFactory])
+  (:import [java.io ObjectInputStream ObjectOutputStream FileInputStream FileOutputStream File]))
+
+(defn createTwitterInstance
+  "Return twitter4j.Twitter Instance with setting consumer key and consumer secret"
+  [consumer-key
+   consumer-secret]
+  (let [twitter (. (twitter4j.TwitterFactory.) getInstance)]
+    (do
+      (. twitter setOAuthConsumer consumer-key consumer-secret)
+      twitter)))
+
+(defn authorizeOAuth
+  "Return access token with oauth-authorization"
+  [twitter
+   output-url-fun
+   input-pin]
+  (let [token (. twitter getOAuthRequestToken)]
+    (do
+      (output-url-fun (. token getAuthorizationURL))
+      (. twitter getOAuthAccessToken token (if (fn? input-pin) (input-pin) input-pin)))))
+
+(defn saveAccessToken
+  "Save AccessToken"
+  [access-token
+   file-name]
+  (with-open [os (new ObjectOutputStream (new FileOutputStream (new File file-name)))]
+    (. os writeObject access-token)))
+
+(defn loadAccessToken
+  "Load AccessToken"
+  [twitter
+   file-name]
+  (with-open [is (new ObjectInputStream (new FileInputStream (new File file-name)))]
+      (. twitter setOAuthAccessToken (. is readObject))))
+
---
+++ test/TwitterMemorizerLib/test/OauthTest.clj
@@ -0,0 +1,3 @@
+(ns TwitterMemorizerLib.test.OauthTest
+  (:use [TwitterMemorizerLib.Oauth])
+  (:use [clojure.test]))
