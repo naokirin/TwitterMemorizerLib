3f3468a39ffd92420e590ef2205b7de6a8b369af
naokirin <malfunctional-ai@sings.jp>
2011-12-03 18:18:01 +0900

    DSLのテストを追加した

--- src/TwitterMemorizerLib/DSL.clj
+++ src/TwitterMemorizerLib/DSL.clj
@@ -16,8 +16,8 @@
       (let [mp (merge m (hash-map :regex-name (-> coll first first)
                                      :screenname-regex-seq (-> coll first rest first)
                                      :tweet-regex-seq (-> coll first rest second)))]
-        (expand-set mp (rest coll)))
-      m))
+      (expand-set mp (rest coll)))
+    m))
   ([coll] (expand-set #{} coll)))
 
 (defmacro- defmemo
--- test/TwitterMemorizerLib/test/DSL.clj
+++ test/TwitterMemorizerLib/test/DSL.clj
@@ -1,3 +1,16 @@
 (ns TwitterMemorizerLib.test.DSL
   (:use [TwitterMemorizerLib.DSL])
   (:use [clojure.test]))
+(defmacro with-private-fns [[ns fns] & tests]
+  "Refers private fns from ns and runs tests in context."
+    `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2)) [] fns)
+         ~@tests))
+
+(with-private-fns [TwitterMemorizerLib.DSL [expand-set]]
+  (deftest DSL
+    (testing "DSL."
+      (testing "expand-setは"
+        (testing "[]を渡すと#{}を返す"
+          (is (= #{} (expand-set []))))
+        (testing "[['a' 'b' 'c']]を渡すと[{:regex-name 'a', :screenname-regex-seq 'b', :tweet-regex-seq} 'c'}]を返す"
+          (is (= #{{:regex-name "a", :screenname-regex-seq "b", :tweet-regex-seq "c"}} (expand-set [["a" "b" "c"]]))))))))
