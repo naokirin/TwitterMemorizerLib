a704e6f0442603fd3250c87ece09a43ad6188d27
naokirin <malfunctional-ai@sings.jp>
2011-12-05 00:20:59 +0900

    DSL.cljのテストを一部追加した

--- test/TwitterMemorizerLib/test/DSL.clj
+++ test/TwitterMemorizerLib/test/DSL.clj
@@ -12,5 +12,11 @@
       (testing "expand-setは"
         (testing "[]を渡すと#{}を返す"
           (is (= #{} (expand-set []))))
-        (testing "[['a' 'b' 'c']]を渡すと[{:regex-name 'a', :screenname-regex-seq 'b', :tweet-regex-seq} 'c'}]を返す"
-          (is (= #{{:regex-name "a", :screenname-regex-seq "b", :tweet-regex-seq "c"}} (expand-set [["a" "b" "c"]]))))))))
+        (testing "[['a' 'b' 'c']]を渡すと#{{:regex-name 'a', :screenname-regex-seq 'b', :tweet-regex-seq 'c'}}を返す"
+          (is (= #{{:regex-name "a", :screenname-regex-seq "b", :tweet-regex-seq "c"}} (expand-set [["a" "b" "c"]]))))
+        (testing "[[1 2 3]]を渡すと#{{:regex-name 1, :screenname-regex-seq 2, :tweet-regex-seq 3}}を返す"
+          (is (= #{{:regex-name 1, :screenname-regex-seq 2, :tweet-regex-seq 3}} (expand-set [[1 2 3]]))))
+        (testing "[[1 2 3] [4 5 6]]を渡すと#{{:regex-name 1, :screenname-regex-seq 2, :tweet-regex-seq 3}
+                                          {:regex-name 4, :screenname-regex-seq 5, :tweet-regex-seq 6}}を返す"
+          (is (= #{{:regex-name 1, :screenname-regex-seq 2, :tweet-regex-seq 3}
+                   {:regex-name 4, :screenname-regex-seq 5, :tweet-regex-seq 6}} (expand-set [[1 2 3] [4 5 6]]))))))))
