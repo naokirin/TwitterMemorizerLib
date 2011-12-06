63e3a17992565d7c34d7a94cf663be2d35438d34
naokirin <malfunctional-ai@sings.jp>
2011-12-07 05:04:22 +0900

    DSL.cljの関数の説明を記述した

--- src/TwitterMemorizerLib/DSL.clj
+++ src/TwitterMemorizerLib/DSL.clj
@@ -4,18 +4,20 @@
 (def ^{:private true} f nil)
 
 (defn read-file
+  "Read DSL file"
   ([file]
     (try (binding [*ns* (the-ns 'TwitterMemorizerLib.DSL)]
       (load-file file) f)
       (catch java.io.FileNotFoundException _))))
 
 (defn- expand-set
+  "Expand DSL set"
   ([m coll]
     (if (not-empty coll)
       (let [c (first coll),
             mp (merge m (hash-map :regex-name (-> c first)
-                                     :screenname-regex-seq (-> c rest first)
-                                     :tweet-regex-seq (-> c rest second)))]
+                                    :screenname-regex-seq (-> c rest first)
+                                    :tweet-regex-seq (-> c rest second)))]
         (expand-set mp (rest coll)))
     m))
   ([coll] (expand-set #{} coll)))
